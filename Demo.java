b560d2a8bfba4e6ddbfac5756216203f7e9e646a
Ojas Kharke <ojaskharke@gmail.com>
2025-07-26 14:13:32 +0530

    Added notification

--- Demo.java
+++ Demo.java
@@ -2,4 +2,9 @@ class Demo{
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+     public void payment(){
+        System.out.println("Payment Method: ");
+     }
+
 }
